3a8f64cde3c9c6d7126f72a180d96ef52b710766
xianglong123 <xianglong1233@163.com>
2021-07-30 17:40:39 +0800

    + cloudalibaba.config

---
+++ cloudalibaba-config-naocs-client3377/src/main/java/com/cas/controller/ConfigClientController.java
@@ -0,0 +1,26 @@
+package com.cas.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xiang_long
+ * @version 1.0
+ * @date 2021/7/30 5:30 下午
+ * @desc
+ */
+@RestController
+@RefreshScope // 支持nacos的动态刷新功能
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+
+}
